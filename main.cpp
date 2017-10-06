fc19ced395aa9d946420eef35b768f98f48a3dbf
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2017-10-06 14:22:26 -0300

    Adding support for playing a default video

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QMediaPlayer>
+#include <QVideoWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+//    MainWindow w;
+//    w.show();
+
+    QVideoWidget* videoWidget = new QVideoWidget();
+    videoWidget->setGeometry(100, 100, 300, 400);
+
+    QMediaPlayer* mediaPlayer = new QMediaPlayer();
+    mediaPlayer->setVideoOutput(videoWidget);
+    mediaPlayer->setMedia(QUrl::fromLocalFile("/home/martin/Vídeos/test.mp4"));
+
+    videoWidget->show();
+    mediaPlayer->play();
 
     return a.exec();
 }
